b6de34b9c5a678bc638c468dfe84fb5db10116c7
Luciano Duarte <lucianoduarterosa@hotmail.com>
2024-08-23 19:00:14 -0300

    Pagina de dash terminada

--- BackEnd/utils/converter.js
+++ BackEnd/utils/converter.js
@@ -35,10 +35,8 @@ class Converter {
     nextMonthTenth(dateString) {
         const date = new Date(dateString);
 
-        // Incrementa o mês em 1
         date.setMonth(date.getMonth() + 1);
 
-        // Define o dia como 10
         date.setDate(10);
 
         const year = date.getFullYear();
@@ -55,14 +53,11 @@ class Converter {
     toMySQLDate(dateInput = new Date()) {
         let date;
 
-        // Verificar se a entrada é uma instância de Date
         if (dateInput instanceof Date) {
             date = dateInput;
         } else if (typeof dateInput === 'string') {
-            // Tentar criar um objeto Date a partir da string
             date = new Date(dateInput);
 
-            // Verificar se a data criada é inválida
             if (isNaN(date.getTime())) {
                 throw new Error('Formato de data inválido.');
             }
@@ -70,7 +65,6 @@ class Converter {
             throw new Error('Entrada deve ser uma string ou um objeto Date.');
         }
 
-        // Formatar a data no formato YYYY-MM-DD HH:MM:SS
         return format(date, 'yyyy-MM-dd HH:mm:ss');
     }
 
@@ -78,10 +72,8 @@ class Converter {
 
     mySQLToFront(dateStr) {
         try {
-            // Parse a data do formato MySQL
             let parsedDate = parse(dateStr, 'yyyy-MM-dd HH:mm:ss', new Date());
 
-            // Se o tempo estiver no formato sem segundos, tentamos parsear novamente
             if (isNaN(parsedDate)) {
                 parsedDate = parse(dateStr, 'yyyy-MM-dd', new Date());
             }
